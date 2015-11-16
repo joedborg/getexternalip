c02f7204df9cb0877a70d7772408a0a881235311
Joseph David Borġ <joe@josephb.org>
2015-11-16 12:45:25 +0000

    Manually taking pull request from tgulacsi

--- getexternalip.go
+++ getexternalip.go
@@ -1,12 +1,18 @@
 package getexternalip
 
 import (
+	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
+// GetExternalIP returns your external IP address as returned by http://echoip.com.
 func GetExternalIP() (string, error) {
-	resp, err := http.Get("http://echoip.com")
+	url := "http://echoip.com"
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", fmt.Errorf("GET %q: %v", url, err)
+	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	return string(body), err
